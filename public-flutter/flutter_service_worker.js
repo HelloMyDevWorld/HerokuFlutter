dc99b084b3f40e7a4c19e05fdc930d13a25c7925
Patryk Sadowski <pasad@ciklum.com>
2020-05-14 11:42:48 +0200

    meals

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -1,17 +1,21 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "c6772928aa819ed8a8a332b108fccfdf",
-"assets/assets/fonts/Montserrat-Bold.ttf": "d3085f686df272f9e1a267cc69b2d24f",
-"assets/assets/fonts/Montserrat-Regular.ttf": "07689d4eaaa3d530d58826b5d7f84735",
-"assets/assets/images/lp_image.png": "5aad4d4216174954f5228d7d268546e8",
-"assets/FontManifest.json": "957423d262d0760a97e6d1ba8e4280e1",
+  "assets/AssetManifest.json": "394a3545c78d990e4e0309bbe7989345",
+"assets/assets/fonts/Raleway-Black.ttf": "3469d4a9bf3b8f9db8f3e5c69e3fce8e",
+"assets/assets/fonts/Raleway-Bold.ttf": "2f99a85426a45e0c7f8707aae53af803",
+"assets/assets/fonts/Raleway-Regular.ttf": "84abe14c9756256a4b91300ba3e4ec62",
+"assets/assets/fonts/RobotoCondensed-Bold.ttf": "141d6bca9dd80c70f193e15e44e196e9",
+"assets/assets/fonts/RobotoCondensed-Italic.ttf": "198e3f07ee8b96ece655b7c4b4b3ce24",
+"assets/assets/fonts/RobotoCondensed-Light.ttf": "a60a2f5cb89d9def151ac47b377a1f09",
+"assets/assets/fonts/RobotoCondensed-Regular.ttf": "9c6aac5ea7419fbe344b1e8f6cc10dac",
+"assets/FontManifest.json": "306b4451fb0d9b462854de1ab655fd98",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
 "index.html": "523e8344930597d4256b511ae9095e4b",
 "/": "523e8344930597d4256b511ae9095e4b",
-"main.dart.js": "79c97fdceb684666872522fa5e830a6b"
+"main.dart.js": "f9068fda568ca322c99969f5bc21c3b7"
 };
 
 self.addEventListener('activate', function (event) {
